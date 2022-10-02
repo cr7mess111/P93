3cee152135b99ad378ffad0184abdd0a2174b5c1
cr7mess111 <85866296+cr7mess111@users.noreply.github.com>
2022-10-02 10:05:35 +0800

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAhFFwhJSg5jinjDase7OK2sPIzKoIguRQ",
+    authDomain: "project93-e1ae6.firebaseapp.com",
+    projectId: "project93-e1ae6",
+    storageBucket: "project93-e1ae6.appspot.com",
+    messagingSenderId: "880952465327",
+    appId: "1:880952465327:web:1d5bbc248ad368f3ea415a"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
